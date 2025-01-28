f820925f472f1ff7edd6ef00958c45edc336517b
nick0995 <neeraj.chauhan.49@gmail.com>
2025-01-28 19:44:33 +0530

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-function Header() {
-  return(
-    <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-    </header>
-  )
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(3)];
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
 }
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
