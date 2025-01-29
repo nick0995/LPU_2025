cf46ad468a34ce6953e22201e3f45af2e98b4e5a
nick0995 <neeraj.chauhan.49@gmail.com>
2025-01-29 19:49:51 +0530

    event fn

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import CoreConcepts from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const handleSelect = () => {
-    console.log("Hellow world-- select method");
+  const handleSelect = (selected) => {
+    // selected --> componenets, jsx, props, state
+    console.log(selected);
   };
 
   return (
@@ -28,10 +29,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
         </section>
       </main>
