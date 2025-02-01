621e5a768b7f6a61d4162f4115d81fc076d98eb1
nick0995 <neeraj.chauhan.49@gmail.com>
2025-02-01 19:29:31 +0530

    2d-matrix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   console.log("APP COMPONENT RENDERED");
@@ -10,7 +11,7 @@ function App() {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
       LOG
     </main>
