191e9304e35cbc4603257e84d1118e4422303093
Neeraj <neeraj04@nagarro.com>
2025-02-14 20:04:21 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import HomePage from './pages/Home';
-import ProductPage from './pages/Product';
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Product";
 
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductPage />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage />},
-  { path: '/products', element: <ProductPage /> }
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductPage /> },
 ]);
 
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <RouterProvider  router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
